281ed82a55b6ba6b31b10d2e13df9c21f18aa722
John Stultz <john.stultz@linaro.org>
2014-03-28 16:23:48 -0700

    ANDROID: netfilter: Build fixups - kuid/kguid changes & xt_socket_get/put_sk
    Fix up build kuid/kguid build issues in netfilter code.
    
    Also re-add the xt_socket_get/put_sk interfaces needed by xt_qtaguid.
    
    Change-Id: I7027fb840e109785bddffe8ea717b8d018b26d82
    Signed-off-by: John Stultz <john.stultz@linaro.org>

--- include/uapi/linux/netfilter/xt_socket.h
+++ include/uapi/linux/netfilter/xt_socket.h
@@ -26,11 +26,10 @@ struct xt_socket_mtinfo3 {
 			   | XT_SOCKET_NOWILDCARD \
 			   | XT_SOCKET_RESTORESKMARK)
 
-struct sock *xt_socket_lookup_slow_v4(struct net *net,
-				      const struct sk_buff *skb,
-				      const struct net_device *indev);
-struct sock *xt_socket_lookup_slow_v6(struct net *net,
-				      const struct sk_buff *skb,
-				      const struct net_device *indev);
+void xt_socket_put_sk(struct sock *sk);
+struct sock *xt_socket_get4_sk(const struct sk_buff *skb,
+			       struct xt_action_param *par);
+struct sock *xt_socket_get6_sk(const struct sk_buff *skb,
+			       struct xt_action_param *par);
 
 #endif /* _XT_SOCKET_H */
